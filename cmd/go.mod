cc515ddb28b29333c70cdec0caa8a9129aaca284
radoslav.boychev <radoslav.boychev@vegait.rs>
2023-02-22 08:55:10 +0100

    recover middleware with syntax highlighting

---
+++ cmd/go.mod
@@ -0,0 +1,7 @@
+module github.com/radoslavboychev/panic-recover
+
+go 1.18
+
+require github.com/alecthomas/chroma v0.10.0
+
+require github.com/dlclark/regexp2 v1.8.0 // indirect
--- cmd/main.go
+++ cmd/main.go
@@ -1,91 +1,122 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"runtime/debug"
+	"strconv"
+	"strings"
+
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/debug/", sourceCodeHandler)
 	mux.HandleFunc("/panic/", panicDemo)
 	mux.HandleFunc("/panic-after/", panicAfterDemo)
 	mux.HandleFunc("/", hello)
-	log.Fatal(http.ListenAndServe(":3000", recoverMw(mux, true)))
+	log.Fatal(http.ListenAndServe(":3000", devMw(mux)))
 }
 
-// recoverMw
-func recoverMw(app http.Handler, dev bool) http.HandlerFunc {
+func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.FormValue("path")
+	lineStr := r.FormValue("line")
+	line, err := strconv.Atoi(lineStr)
+	if err != nil {
+		line = -1
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	b := bytes.NewBuffer(nil)
+	_, err = io.Copy(b, file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var lines [][2]int
+	if line > 0 {
+		lines = append(lines, [2]int{line, line})
+	}
+	lexer := lexers.Get("go")
+	iterator, err := lexer.Tokenise(nil, b.String())
+	style := styles.Get("github")
+	if style == nil {
+		style = styles.Fallback
+	}
+	formatter := html.New(html.TabWidth(2), html.HighlightLines(lines))
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<style>pre { font-size: 1.2em; }</style>")
+	formatter.Format(w, style, iterator)
+	// _ = quick.Highlight(w, b.String(), "go", "html", "github")
+}
+
+func devMw(app http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
 				log.Println(err)
-				http.Error(w, "Something went wrong - Panic!", http.StatusInternalServerError)
 				stack := debug.Stack()
 				log.Println(string(stack))
-				if !dev {
-					http.Error(w, "Something went wrong", http.StatusInternalServerError)
-					return
-				}
-				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, string(stack))
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Fprintf(w, "<h1>panic: %v</h1><pre>%s</pre>", err, makeLinks(string(stack)))
 			}
 		}()
-		nw := &responseWriter{ResponseWriter: w}
-		nw.flush()
 		app.ServeHTTP(w, r)
 	}
 }
 
-// responseWriter
-type responseWriter struct {
-	http.ResponseWriter
-	writes [][]byte
-	status int
-}
-
-// Write
-func (rw *responseWriter) Write(b []byte) (int, error) {
-	rw.writes = append(rw.writes, b)
-	return len(b), nil
-}
-
-// WriteHeader
-func (rw *responseWriter) WriteHeader(statusCode int) {
-	rw.status = statusCode
-}
-
-// flush
-func (rw *responseWriter) flush() error {
-	if rw.status != 0 {
-		rw.ResponseWriter.WriteHeader(rw.status)
-	}
-	for _, write := range rw.writes {
-		_, err := rw.ResponseWriter.Write(write)
-		if err != nil {
-			return nil
-		}
-	}
-	return nil
-}
-
-// panicDemo triggers panic
 func panicDemo(w http.ResponseWriter, r *http.Request) {
 	funcThatPanics()
 }
 
-// panicAfterDemo
 func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello!</h1>")
 	funcThatPanics()
 }
 
-// funcThatPanics triggers a panic state
 func funcThatPanics() {
-	panic("OHHHHH")
+	panic("Oh no!")
 }
 
-// hello default home page
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Hello!</h1>")
+}
+
+func makeLinks(stack string) string {
+	lines := strings.Split(stack, "\n")
+	for li, line := range lines {
+		if len(line) == 0 || line[0] != '\t' {
+			continue
+		}
+		file := ""
+		for i, ch := range line {
+			if ch == ':' {
+				file = line[1:i]
+				break
+			}
+		}
+		var lineStr strings.Builder
+		for i := len(file) + 2; i < len(line); i++ {
+			if line[i] < '0' || line[i] > '9' {
+				break
+			}
+			lineStr.WriteByte(line[i])
+		}
+		v := url.Values{}
+		v.Set("path", file)
+		v.Set("line", lineStr.String())
+		lines[li] = "\t<a href=\"/debug/?" + v.Encode() + "\">" + file + ":" + lineStr.String() + "</a>" + line[len(file)+2+len(lineStr.String()):]
+	}
+	return strings.Join(lines, "\n")
 }
